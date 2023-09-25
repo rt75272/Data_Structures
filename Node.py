011afeab7ac41691a583fa6bb8676910f1a0e82d
rt75272 <rt75272@gmail.com>
2023-09-25 12:46:34 -0500

    Added Linked Listy

---
+++ Node.py
@@ -0,0 +1,10 @@
+class Node:
+    
+    def __init__(self, data=None):
+        print("Node start")
+        self.data = data
+        self.next = None
+
+    # def __get_node__():
+    #     test_node = Node("42")
+    #     return test_node
